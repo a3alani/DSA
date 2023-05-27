1d275b3ed18e1087015cbe5166ecbbfd5d2ef733
a3alani <a3.alani@gmail.com>
2023-05-27 15:09:31 -0700

    Update singlyLinkedLists.cpp

--- LinkedLists/singlyLinkedLists.cpp
+++ LinkedLists/singlyLinkedLists.cpp
@@ -1,64 +1,31 @@
-// Purpose: To study the implementation of singly linked lists
 /**
 * Singly Linked List
-* Pros:
-* 1. Dynamic size
-* 2. Ease of insertion/deletion
-* 3. Ease of implementation
-* 4. Random access is not allowed
-* 5. Extra memory space for a pointer is required with each element of the list
-* Cons:
-* 1. Random access is not allowed
-* 2. Extra memory space for a pointer is required with each element of the list
-* 3. Not cache friendly. Since array elements are contiguous locations, there is locality of reference which is not there in case of linked lists.
-* 4. Reverse traversing is difficult in singly linked list. We can overcome this by using doubly linked list.
-* 5. Singly linked list requires less memory than doubly linked list.
-* 6. Singly linked list can be traversed only in one direction. Doubly linked list can be traversed in both the directions.
-* 7. The delete operation in singly linked list is more complicated. We need to modify the next pointer of the previous node. In doubly linked list, we can get the previous node using previous pointer.
-* 8. Insertion and deletion take more time.
-* 9. Singly linked list is preferred over doubly linked list when there is memory constraint.
+* - Dynamic size
+* - Ease of insertion/deletion
+* - Extra memory space for a pointer is required with each element of the list
+* - Not cache friendly. Since array elements are contiguous locations, there is locality of reference which is not there in case of linked lists.
+* - Reverse traversing is difficult in singly linked list. We can overcome this by using doubly linked list.
+* - Singly linked list can be traversed only in one direction. Doubly linked list can be traversed in both the directions.
+* - The delete operation in singly linked list is more complicated. We need to modify the next pointer of the previous node. In doubly linked list, we can get the previous node using previous pointer.
+* - Insertion and deletion take more time.
+* - Singly linked list is preferred over doubly linked list when there is memory constraint.
 
-* Operations:
-* 1. Insertion
-* 2. Deletion
-* 3. Traversal
-* 4. Searching
-* 5. Sorting
-* 6. Merging
-* 7. Reversing
-* 8. Copying
-* 9. Concatenation
-* 10. Splitting
-* 11. Circular Singly Linked List
-* 12. Circular Doubly Linked List
-* 13. Doubly Linked List
-* 14. Circular Doubly Linked List
-* 15. Circular Singly Linked List
-* 16. Circular Doubly Linked List
-*
+* Time Complexity: 
+* - Access: O(n)
+* - Search: O(n)
+* - Insertion: O(1)
+* - Deletion: O(1)
+* - Space: O(n)
+
 
 * Applications:
-* 1. Dynamic memory allocation
-* 2. Implemented in stack and queue
-* 3. In undo functionality of softwares
-* 4. Hash tables, Graphs
-* 5. Implemented in adjacency list of graphs
-* 6. Implemented in separate chaining which is present certain implementations of hash tables
-* 7. Implemented in maintaining directory of names
-* 8. Implemented in performing arithmetic operations on long integers
-* 9. Implemented in performing arithmetic operations on polynomials
-* 10. Implemented in performing arithmetic operations on sparse matrices
-* 11. Implemented in performing arithmetic operations on polynomials
+* - Dynamic memory allocation
+* - Implemented in stack and queue
+* - Hash tables, Graphs
+* - Implemented in adjacency list of graphs
+* - Implemented in separate chaining which is present certain implementations of hash tables
 * 
-*
-* References:
-* 1. https://www.geeksforgeeks.org/data-structures/linked-list/
-* 2. https://www.geeksforgeeks.org/linked-list-set-1-introduction/
-* 3. https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
-* 4. https://www.geeksforgeeks.org/linked-list-set-3-deleting-node/
-* 5. https://www.geeksforgeeks.org/linked-list-set-4-inserting-a-node/
-* 
-*
+
 * Class vs Struct in C++
 * The only difference between struct and class keyword is that 
 * members of struct are public by default and members of class are private by default.
